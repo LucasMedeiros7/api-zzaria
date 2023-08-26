3534b84549cc0eb88e6e81155358fb31801c1858
Lucas Medeiros <103211800+LucasMedeiros7@users.noreply.github.com>
2023-08-26 12:24:06 -0300

    feat: enhance Pizza entity with encapsulated attributes and setters

--- src/modules/pizzas/domain/Pizza.ts
+++ src/modules/pizzas/domain/Pizza.ts
@@ -1,18 +1,44 @@
+/* eslint-disable no-underscore-dangle */
 export class Pizza {
-  constructor(
-    private name: string,
-    private price: number,
-    private ingredients: string[],
-  ) {
-    if (this.price <= 0) throw new Error('Price must be positive');
-    this.price = price;
+  private _name: string;
+
+  private _price: number;
+
+  private _ingredients: string[];
+
+  constructor(name: string, price: number, ingredients: string[]) {
+    if (price <= 0) {
+      throw new Error('Price must be positive');
+    }
+    this._name = name;
+    this._price = price;
+    this._ingredients = ingredients;
   }
 
-  getValues() {
-    return {
-      name: this.name,
-      price: this.price,
-      ingredients: this.ingredients,
-    };
+  get name(): string {
+    return this._name;
+  }
+
+  set name(name: string) {
+    this._name = name;
+  }
+
+  get price(): number {
+    return this._price;
+  }
+
+  set price(price: number) {
+    if (price <= 0) {
+      throw new Error('Price must be positive');
+    }
+    this._price = price;
+  }
+
+  get ingredients(): string[] {
+    return this._ingredients;
+  }
+
+  set ingredients(ingredients: string[]) {
+    this._ingredients = ingredients;
   }
 }
--- src/modules/pizzas/domain/PizzaRepository.ts
+++ src/modules/pizzas/domain/PizzaRepository.ts
@@ -2,4 +2,6 @@ import { Pizza } from './Pizza';
 
 export interface PizzaRepository {
   insert(pizza: Pizza): Promise<void>;
+  findByName(name: string): Promise<Pizza | undefined>
+  delete(name: string): Promise<void>
 }
