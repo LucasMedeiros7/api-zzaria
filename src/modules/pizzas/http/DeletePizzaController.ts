889527a0e49a8f219f7a4330cf969dc2bf8ef0fc
Lucas Medeiros <103211800+LucasMedeiros7@users.noreply.github.com>
2023-08-28 18:49:35 -0300

    feat: DeletePizzaController

---
+++ src/modules/pizzas/http/DeletePizzaController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import { DeletePizzaUseCase } from '../application/usecases';
+
+export class DeletePizzaController {
+  constructor(private deletePizzaUseCase: DeletePizzaUseCase) { }
+
+  async delete(request: Request, response: Response): Promise<Response> {
+    const { pizzaId } = request.params;
+    await this.deletePizzaUseCase.execute(pizzaId);
+    return response.status(202).json({ message: `Pizza with id: ${pizzaId} was deleted` });
+  }
+}
